5813c8fecda093c397ba6e58275977dcd2cf2b7d
Genevieve Osuorji <genosuorji@yahoo.com>
2024-07-21 02:07:02 +0100

    Added breadcrumbs and alert to layout

--- example-views/yiisoft/yii2-app/layouts/content.php
+++ example-views/yiisoft/yii2-app/layouts/content.php
@@ -1,8 +1,20 @@
 <?php
     /** @var string $content */
+
+use app\widgets\Alert;
+use yii\bootstrap5\Breadcrumbs;
 ?>
 <div class="content">
     <div class="container-fluid">
+        <?php if (!empty($this->params['breadcrumbs'])): ?>
+            <?= Breadcrumbs::widget([
+                'homeLink' => [
+                    'label' => Yii::t('app', 'Home'),
+                    'url' => ['default/index'],
+                ],
+                'links' => $this->params['breadcrumbs']]) ?>
+        <?php endif ?>
+        <?= Alert::widget() ?>
         <?php $directoryAsset; ?>
         <?= $content; ?>
     </div>
